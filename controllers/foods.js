c6413f1fa69da8b2b9de93140f979e870b4b6fe5
Tia Piol <tiarox02@yahoo.com>
2025-07-24 14:31:46 -0700

    conceptualize the new route, define the new route, and render the new route

--- controllers/foods.js
+++ controllers/foods.js
@@ -6,10 +6,13 @@ const User = require("../models/user.js")
 // Router Logic
 
 // GET /users/:userId/foods
-
 router.get("/", (req, res) => {
   res.render("foods/index.ejs");
 });
 
+// GET /users/:userId/foods/new
+router.get("/new", async (req, res) => {
+  res.render("foods/new.ejs");
+});
 
 module.exports = router;

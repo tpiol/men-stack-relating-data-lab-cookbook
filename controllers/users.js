de1c6bbdfdf6594eb74d3b13bdb3183d29bcebcb
Tia Piol <tiarox02@yahoo.com>
2025-07-25 11:53:39 -0700

    view other users pantry

--- controllers/users.js
+++ controllers/users.js
@@ -11,14 +11,23 @@ router.get("/", async (req, res) => {
             allUsers,
         });
     } catch (error) {
-    console.log(error);
-    res.redirect("/")
-}
+        console.log(error);
+        res.redirect("/")
+    }
 });
 
 // users/show
-router.get("/:userId", (req, res) => {
-    res.send(`Here is your user: ${req.params.userId}`)
+router.get("/:userId", async (req, res) => {
+    try {
+        const otherUser = await User.findById(req.params.userId);
+        res.render("users/show.ejs", {
+            otherUser: otherUser.pantry,
+            otherUsername: otherUser.username,
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect("/");
+    }
 });
 
 module.exports = router;

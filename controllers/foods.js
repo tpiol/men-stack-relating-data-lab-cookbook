d4f9d96aee5196bfc0b8bb6550750d62cfb7dd02
Tia Piol <tiarox02@yahoo.com>
2025-07-24 19:04:00 -0700

    start of show page and delete functionality page

--- controllers/foods.js
+++ controllers/foods.js
@@ -37,6 +37,24 @@ router.post("/", async (req, res) => {
   }
 });
 
+// GET /controllers/foods.js
+router.get("/:foodsId", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const food = currentUser.pantry.id(req.params.foodId);
+    res.render("foods/show.ejs")
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
+})
 
+// DELETE /users/:userId/foods/:foodId
+router.delete("/:foodId", async (req, res) => {
+  const currentUser = await User.findById(req.session.user._id);
+  currentUser.pantry.id(req.params.foodId).deleteOne();
+  await currentUser.save();
+  res.redirect(`/users/${currentUser._id}/foods`)
+});
 
 module.exports = router;
